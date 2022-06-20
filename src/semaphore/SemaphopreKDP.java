d4e42421aec520ff2bca9f8940d90239adaa7e0f
Rasa <stojanovicrasa4@gmail.com>
2022-06-20 19:42:53 +0200

    Semaphore, changes

--- src/semaphore/SemaphopreKDP.java
+++ src/semaphore/SemaphopreKDP.java
@@ -6,4 +6,22 @@ public class SemaphopreKDP {
 	public SemaphopreKDP(int init) {
 		this.s = init;
 	}
+	
+	public synchronized void semWait() {
+		while(s == 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		s--;
+	}
+	
+	public synchronized void semSignal() {
+		s++;
+		notify();
+	}
 }
+
